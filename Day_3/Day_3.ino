f5833da42935e7e1b07d589060843c3185ba1ca3
MewPk <milzaza123@gmail.com>
2022-08-23 13:32:51 +0700

    Successful Day3

---
+++ Day_3/Day_3.ino
@@ -0,0 +1,186 @@
+
+void setup() {
+ 
+}
+
+void loop() {
+  
+pinMode(2,OUTPUT);
+ digitalWrite(2,LOW);
+ pinMode(3,OUTPUT);
+ digitalWrite(3,HIGH);
+ pinMode(4,INPUT);
+// LED 1
+delay(500);
+
+pinMode(2,OUTPUT);
+ digitalWrite(2,LOW);
+ pinMode(3,OUTPUT);
+ digitalWrite(3,LOW);
+ pinMode(4,INPUT);
+// LED 1
+
+ 
+ pinMode(3,OUTPUT);
+ digitalWrite(3,LOW);
+ pinMode(4,OUTPUT);
+ digitalWrite(4,HIGH);
+ pinMode(2,INPUT);
+//LED2 
+delay(500);
+
+ pinMode(3,OUTPUT);
+ digitalWrite(3,LOW);
+ pinMode(4,OUTPUT);
+ digitalWrite(4,LOW);
+ pinMode(2,INPUT);
+//LED2 
+
+
+ pinMode(2,OUTPUT);
+ digitalWrite(2,HIGH);
+ pinMode(3,OUTPUT);
+ digitalWrite(3,LOW);
+ pinMode(4,INPUT);
+ //LED3
+ delay(500);
+ 
+ pinMode(2,OUTPUT);
+ digitalWrite(2,LOW);
+ pinMode(3,OUTPUT);
+ digitalWrite(3,LOW);
+ pinMode(4,INPUT);
+ //LED3
+ 
+
+ pinMode(3,OUTPUT);
+ digitalWrite(3,HIGH);
+ pinMode(4,OUTPUT);
+ digitalWrite(4,LOW);
+ pinMode(2,INPUT);
+//LED4 
+delay(500);
+
+ pinMode(3,OUTPUT);
+ digitalWrite(3,LOW);
+ pinMode(4,OUTPUT);
+ digitalWrite(4,LOW);
+ pinMode(2,INPUT);
+//LED4 
+
+
+pinMode(3,INPUT);
+ pinMode(2,OUTPUT);
+ digitalWrite(2,HIGH);
+ pinMode(4,OUTPUT);
+ digitalWrite(4,LOW);
+ 
+//LED5
+delay(500);
+
+pinMode(3,INPUT);
+ pinMode(2,OUTPUT);
+ digitalWrite(2,LOW);
+ pinMode(4,OUTPUT);
+ digitalWrite(4,LOW);
+ 
+//LED5
+
+
+
+pinMode(3,INPUT);
+ pinMode(2,OUTPUT);
+ digitalWrite(2,LOW);
+ pinMode(4,OUTPUT);
+ digitalWrite(4,HIGH);
+ 
+//LED6
+delay(500);
+
+pinMode(3,INPUT);
+ pinMode(2,OUTPUT);
+ digitalWrite(2,LOW);
+ pinMode(4,OUTPUT);
+ digitalWrite(4,LOW);
+ 
+//LED6
+
+
+
+
+ // กลับ
+
+
+ 
+pinMode(3,INPUT);
+ pinMode(2,OUTPUT);
+ digitalWrite(2,HIGH);
+ pinMode(4,OUTPUT);
+ digitalWrite(4,LOW);
+ 
+//LED5
+delay(500);
+
+pinMode(3,INPUT);
+ pinMode(2,OUTPUT);
+ digitalWrite(2,LOW);
+ pinMode(4,OUTPUT);
+ digitalWrite(4,LOW);
+ 
+//LED5
+
+
+
+ pinMode(3,OUTPUT);
+ digitalWrite(3,HIGH);
+ pinMode(4,OUTPUT);
+ digitalWrite(4,LOW);
+ pinMode(2,INPUT);
+//LED4 
+delay(500);
+
+ pinMode(3,OUTPUT);
+ digitalWrite(3,LOW);
+ pinMode(4,OUTPUT);
+ digitalWrite(4,LOW);
+ pinMode(2,INPUT);
+//LED4 
+
+ 
+
+ pinMode(2,OUTPUT);
+ digitalWrite(2,HIGH);
+ pinMode(3,OUTPUT);
+ digitalWrite(3,LOW);
+ pinMode(4,INPUT);
+ //LED3
+ delay(500);
+ 
+ pinMode(2,OUTPUT);
+ digitalWrite(2,LOW);
+ pinMode(3,OUTPUT);
+ digitalWrite(3,LOW);
+ pinMode(4,INPUT);
+ //LED3
+ 
+
+ 
+ pinMode(3,OUTPUT);
+ digitalWrite(3,LOW);
+ pinMode(4,OUTPUT);
+ digitalWrite(4,HIGH);
+ pinMode(2,INPUT);
+//LED2 
+delay(500);
+
+ pinMode(3,OUTPUT);
+ digitalWrite(3,LOW);
+ pinMode(4,OUTPUT);
+ digitalWrite(4,LOW);
+ pinMode(2,INPUT);
+//LED2 
+
+
+
+ 
+}
